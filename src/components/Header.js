5d289d37595a4a82eff7f5c0c7cea0ffe747903c
Yajahira <yari@yvelazquez.com>
2022-07-06 17:49:03 -0400

    tried to get the icons to show up

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import LoginButton from './LoginButton';
 import Navdropdown from './Navdropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 //import { Image } from 'react-bootstrap';
 //import ReactDOM from 'react-dom/client';
 
@@ -9,6 +12,10 @@ function Header() {
 
   return (
     <>
+      <FontAwesomeIcon icon="facebook" />
+      {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+      <FontAwesomeIcon icon="fa-brands fa-pinterest" />
+      <FontAwesomeIcon icon="fa-solid fa-link" /> */}
       <Navdropdown />
       <LoginButton />
       <img src="holder.js/100px180" />
